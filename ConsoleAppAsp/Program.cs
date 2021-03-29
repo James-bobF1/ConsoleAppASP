6a2ad6b40032ff8f4b6e4b24c0722dd971b7d1c6
dmitry.khlebnikov <james-bob@mail.ru>
2021-03-30 02:42:38 +0300

    test console.readline/writeline

--- ConsoleAppAsp/Program.cs
+++ ConsoleAppAsp/Program.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿//using System;
+//Если мы не используем пространство имен, нам необходимо явно обращаться к его членам.
 
 namespace ConsoleAppAsp
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {;
+            System.Console.WriteLine("What is your name?");
+            string name = System.Console.ReadLine();
+            System.Console.WriteLine($"Привет, {name}, сегодня {System.DateTime.Now}");
+            System.Console.ReadKey();
         }
+        
     }
 }
