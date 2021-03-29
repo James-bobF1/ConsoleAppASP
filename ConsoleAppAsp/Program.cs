65a452ee40887b345c43e2bf17c5b1a7abed96c2
dmitry.khlebnikov <james-bob@mail.ru>
2021-03-30 02:24:13 +0300

    init app

---
+++ ConsoleAppAsp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppAsp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
