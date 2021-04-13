a17f63dae0197bf0fef8bb43f4908e3b4f6a3f58
dmitry.khlebnikov <james-bob@mail.ru>
2021-04-13 23:15:16 +0300

    version 2

--- ConsoleAppAsp/Program.cs
+++ ConsoleAppAsp/Program.cs
@@ -6,7 +6,31 @@ namespace ConsoleAppAsp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task1();
+            Task2();
+            Task3();
+            Task4();
+            Task5();
+        }
+        static void Task1()
+        {
+
+        }
+        static void Task2()
+        {
+
+        }
+        static void Task3()
+        {
+
+        }
+        static void Task4()
+        {
+
+        }
+        static void Task5()
+        {
+
         }
     }
 }
