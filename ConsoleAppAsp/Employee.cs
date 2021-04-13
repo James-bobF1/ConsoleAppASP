237017a352adb3681bcfb62a877753c3a7694405
dmitry.khlebnikov <james-bob@mail.ru>
2021-04-14 02:45:26 +0300

    geekbrains.introduction.lesson_5

---
+++ ConsoleAppAsp/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppAsp
+{
+    class Employee
+    {
+        //ФИО, должность, email, телефон, зарплата, возраст
+        string Fio;
+        string Post;
+        string Email;
+        string Phone;
+        float Salary;
+        decimal age;
+        public decimal Age 
+        { 
+            set
+            {
+                age = (value > 0 && value < 200) ? value : 0;
+            }
+            get 
+            {
+                return age;
+            } 
+        }
+        public Employee(string fio, string post,string email, string phone, float salary, decimal age)
+        {
+            Fio = fio;
+            Post = post;
+            Email = email;
+            Phone = phone;
+            Salary = salary;
+            Age = age;
+        }
+        public override string ToString()
+        {
+            return $"{Fio} {Post} {Email} {Phone} Salary {Salary} Age {Age}";
+        }
+    }
+}
